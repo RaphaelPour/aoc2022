ce21ff524435e8206e29036166e606f0a5e12509
Raphael Pour <info@raphaelpour.de>
2022-12-01 06:43:32 +0100

    01: simplify with Sum(MaxN(1)) and Sum(MaxN(3))

--- days/01/part.go
+++ days/01/part.go
@@ -2,29 +2,13 @@ package main
 
 import (
 	"fmt"
-	"sort"
 
 	"github.com/RaphaelPour/stellar/input"
+	"github.com/RaphaelPour/stellar/math"
 	"github.com/RaphaelPour/stellar/strings"
 )
 
-func part1(data []string) int {
-	maxCalories := 0
-	currentCalories := 0
-	for i, row := range data {
-		if row == "" || i == len(data)-1 {
-			if currentCalories > maxCalories {
-				maxCalories = currentCalories
-			}
-			currentCalories = 0
-			continue
-		}
-		currentCalories += strings.ToInt(row)
-	}
-	return maxCalories
-}
-
-func part2(data []string) int {
+func maxCalories(data []string, n int) int {
 	calories := make([]int, 0)
 	currentCalories := 0
 	for i, row := range data {
@@ -36,18 +20,15 @@ func part2(data []string) int {
 		currentCalories += strings.ToInt(row)
 	}
 
-	sort.Ints(calories)
-
-	l := len(calories)
-	return calories[l-1] + calories[l-2] + calories[l-3]
+	return math.Sum(math.MaxN(calories, n))
 }
 
 func main() {
 	data := input.LoadString("input")
 
 	fmt.Println("== [ PART 1 ] ==")
-	fmt.Println(part1(data))
+	fmt.Println(maxCalories(data, 1))
 
 	fmt.Println("== [ PART 2 ] ==")
-	fmt.Println(part2(data))
+	fmt.Println(maxCalories(data, 3))
 }
