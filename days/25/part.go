5528684288315281d871dda304684c46b067b446
Raphael Pour <info@raphaelpour.de>
2022-12-26 09:18:36 +0100

    25: solve

--- days/25/part.go
+++ days/25/part.go
@@ -1,26 +1,72 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/RaphaelPour/stellar/input"
+	"github.com/RaphaelPour/stellar/math"
 )
 
-func part1(data []string) int {
-    return 0
+var (
+	snafuMap = map[string]int{
+		"=": -2,
+		"-": -1,
+		"0": 0,
+		"1": 1,
+		"2": 2,
+	}
+
+	decMap = map[int]string{
+		-2: "=",
+		-1: "-",
+		0:  "0",
+		1:  "1",
+		2:  "2",
+	}
+)
+
+func snafu2Dec(n string) int {
+	result := 0
+	for i := len(n) - 1; i >= 0; i-- {
+		result += math.Pow(5, len(n)-i-1) * snafuMap[string(n[i])]
+	}
+	return result
+}
+
+func dec2Snafu(n int) string {
+	result := ""
+	translate := []string{"0", "1", "2", "=", "-"}
+	for n != 0 {
+		m := n % 5
+		n /= 5
+		if m > 2 {
+			n++
+		}
+		result = translate[m] + result
+	}
+	return result
+}
+
+func part1(data []string) string {
+	sum := 0
+	for _, line := range data {
+		fmt.Println(line)
+		sum += snafu2Dec(line)
+	}
+	fmt.Println(sum)
+	return dec2Snafu(sum)
 }
 
 func part2(data []string) int {
-    return 0
+	return 0
 }
 
 func main() {
-	// data := input.LoadString("input")
-	// data := input.LoadDefaultInt()
-	// data := input.LoadInt("input")
-	data := input.LoadDefaultString()
-    
-    fmt.Println("== [ PART 1 ] ==")
-    fmt.Println(part1(data))
+	data := input.LoadString("input")
 
-    // fmt.Println("== [ PART 2 ] ==")
-    // fmt.Println(part2(data))
+	fmt.Println("== [ PART 1 ] ==")
+	fmt.Println(part1(data))
+
+	// fmt.Println("== [ PART 2 ] ==")
+	// fmt.Println(part2(data))
 }
--- days/25/part_test.go
+++ days/25/part_test.go
@@ -1,11 +1,57 @@
 package main
 
 import (
-    "testing"
+	"testing"
 
-        "github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/require"
 )
 
-func TestExample(t *testing.T) {
-    // require.Equal(t, 0, part1([]string{})
+func TestSnafu2Dec(t *testing.T) {
+	for _, data := range []struct {
+		Input    string
+		Expected int
+	}{
+		{"1", 1},
+		{"2", 2},
+		{"1=", 3},
+		{"1-", 4},
+		{"10", 5},
+		{"11", 6},
+		{"12", 7},
+		{"2=", 8},
+		{"2-", 9},
+		{"20", 10},
+		{"1=0", 15},
+		{"1-0", 20},
+		{"1=11-2", 2022},
+		{"1-0---0", 12345},
+		{"1121-1110-1=0", 314159265},
+	} {
+		require.Equal(t, data.Expected, snafu2Dec(data.Input))
+	}
+}
+
+func TestDec2Snafu(t *testing.T) {
+	for _, data := range []struct {
+		Expected string
+		Input    int
+	}{
+		{"1", 1},
+		{"2", 2},
+		{"1=", 3},
+		{"1-", 4},
+		{"10", 5},
+		{"11", 6},
+		{"12", 7},
+		{"2=", 8},
+		{"2-", 9},
+		{"20", 10},
+		{"1=0", 15},
+		{"1-0", 20},
+		{"1=112", 2022},
+		{"1-0---0", 12345},
+		{"1121-1110-1=0", 314159265},
+	} {
+		require.Equal(t, data.Expected, dec2Snafu(data.Input))
+	}
 }
