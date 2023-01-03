26f17738d8b853c21149cec0948e41b63b6aa097
Raphael Pour <info@raphaelpour.de>
2023-01-03 15:59:34 +0100

    01: resolve more rust stuff

--- days/01/part.rs
+++ days/01/part.rs
@@ -7,21 +7,25 @@ fn main() -> io::Result<()> {
     let mut max: i32 = 0;
     let mut sum: i32 = 0;
 
+    // https://doc.rust-lang.org/book/ch09-02-recoverable-errors-with-result.html#propagating-errors
     let file = match File::open("input"){
         Err(e) => return Err(e),
         Ok(file) => file,
     };
     let reader = BufReader::new(file);
     for line in reader.lines() {
-        if let Ok(data) = line {
-            if data == "" {
-                max = cmp::max(max, sum);
-                sum = 0;
-                continue
-            }
-            // https://doc.rust-lang.org/std/primitive.str.html#method.parse
-            sum += data.parse::<i32>().unwrap();
+        let data = match line {
+            Err(e) => return Err(e),
+            Ok(data) => data,
+        };
+
+        if data == "" {
+            max = cmp::max(max, sum);
+            sum = 0;
+            continue
         }
+        // https://doc.rust-lang.org/std/primitive.str.html#method.parse
+        sum += data.parse::<i32>().unwrap();
     }
     
     max = cmp::max(max, sum);
